85d32f5951ec6486d6dafab50fa237e655faf8d8
Ishan Karki@IshanLaptop <karkiishan6295@gmail.com>
2026-04-20 10:14:45 +0545

    fixed errors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'force-module-mime-type',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          const urlPath = req.url.split('?')[0];
+          if (urlPath.endsWith('.jsx') || urlPath.endsWith('.js')) {
+            res.setHeader('Content-Type', 'application/javascript');
+          }
+          next();
+        });
+      }
+    }
+  ],
 })
