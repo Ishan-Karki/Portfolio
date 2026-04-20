031fa99628b9edfa20dfed6dea2ccc177f809d34
Ishan Karki@IshanLaptop <karkiishan6295@gmail.com>
2026-04-20 11:03:25 +0545

    Updated image and lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Lenis from 'lenis';
 import './index.css';
 
 import { Header } from './components/Header';
@@ -8,6 +9,27 @@ import { CaseStudies } from './components/CaseStudies';
 import { Contact } from './components/Contact';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Header />
